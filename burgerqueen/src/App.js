7b1dd5fa56f81ebfb79e8488e8fca668125b2a7b
Alejandra García <agarcia_pacheco@hotmail.com>
2021-04-21 11:38:54 -0500

    primeros pasos

---
+++ burgerqueen/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+//componets
+import Navbar from './components/navbar/navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+
+    </div>
+
+  )
+
+}
+
+export default App;
---
+++ burgerqueen/src/components/navbar/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import './Navbar.css';
+
+function Navbar() {
+	return (
+		<div className="menu">
+			<ul>
+				<li> Logo </li>
+				<li> Mesas </li>
+				<li> Ordenes en proceso </li>
+				<li> Ordenes listas </li>
+				<li> Salir </li>
+			</ul>
+		</div>
+	)
+}
+
+export default Navbar;
