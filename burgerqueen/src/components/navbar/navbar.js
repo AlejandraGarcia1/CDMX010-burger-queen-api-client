b0b1cb8d15f2c2e703dd6a1f92e5d01404ea9e10
Alejandra García <agarcia_pacheco@hotmail.com>
2021-04-21 13:39:12 -0500

    estructura visual del navbar lista

--- burgerqueen/src/components/navbar/navbar.js
+++ burgerqueen/src/components/navbar/navbar.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import './Navbar.css';
+import logo from '../../images/logo.png';
+import salir from '../../images/salir.png';
 
 function Navbar() {
-	return (
-		<div className="menu">
-			<ul>
-				<li> Logo </li>
-				<li> Mesas </li>
-				<li> Ordenes en proceso </li>
-				<li> Ordenes listas </li>
-				<li> Salir </li>
-			</ul>
+	return (	
+		<header>
+		<div className = "logo">
+			<img src = { logo } alt = "burgerLover" className = "burger"/>
 		</div>
+
+		<div className = "nav">
+			<ul>
+				<li><a href= ".." className = "navMenu"> Mesas </a></li>
+				<li><a href= ".." className = "navMenu"> Ordenes en proceso </a></li>
+				<li><a href= ".." className = "navMenu"> Ordenes listas </a></li>
+				<li><a href= ".." ><img src = { salir } alt = "salir" className = "salirApp"/> </a></li>			
+			</ul>		
+		</div>
+		</header>
 	)
 }
 
